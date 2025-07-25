6666aaddb85e58b2c4faabfcfe50c385e9129fbe
But43r <156163071+ImMushroomsWeTrust@users.noreply.github.com>
2025-07-25 08:55:19 +0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="https://immushroomswetrust.github.io/VanLife/" element={<Layout />}>
+        <Route basename="/VanLife" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
