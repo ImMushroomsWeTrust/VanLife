eea502737a28d1fe3decc67a67c1317c6eeda1e3
But43r <156163071+ImMushroomsWeTrust@users.noreply.github.com>
2025-07-25 08:39:32 +0700

    Changed Home URL to GitPages URL

--- index.jsx
+++ index.jsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="https://immushroomswetrust.github.io/VanLife/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
